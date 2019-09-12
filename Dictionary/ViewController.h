9167be9f3e9aac64128eb347d2726626cbc8ae21
AnatolyRyavkin <toryavkin@gmail.com>
2019-09-12 11:18:51 +0300

    Initial Commit

---
+++ Dictionary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dictionary
+//
+//  Created by Anatoly Ryavkin on 12/09/2019.
+//  Copyright © 2019 AnatolyRyavkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
